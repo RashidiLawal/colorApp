7ed08e7cd9c4b40882c0abe4956d18584f71ff24
lawal rashidi <lawalrashidi95@gmail.com>
2022-05-28 09:17:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from "./Input";
+import Square from "./Square";
+import React, { useState } from "react";
 
 function App() {
+const [colorValue, setColorValue] = useState('')
+const [hexValue, setHexValue] = useState('')
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+     <div className="App">
+          <Square colorValue={colorValue} hexValue={hexValue}/>
+          <Input colorValue={colorValue} setColorValue={setColorValue} setHexValue={setHexValue}/> 
     </div>
+
+
+    
   );
 }
 
---
+++ src/Input.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import colorNames from 'colornames';
+
+const Input = ({colorValue, setColorValue, setHexValue}) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault}>
+        <label>
+            Add Color Name:
+        </label>
+        <input
+        autoFocus
+        type="text"
+        placeholder='Add color Name'
+        required
+        value={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value)
+          setHexValue(colorNames(e.target.value))
+        }}
+        />
+    </form>
+  )
+}
+
+export default Input
